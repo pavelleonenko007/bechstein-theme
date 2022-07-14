71aa1e8f9ae50929fe74f9ff1e002398838c2f8c
pavelleonenko007 <pavel.leonenko374@gmail.com>
2022-07-14 16:20:19 +0500

    add header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+  <div class="foo-mom">
+    <div class="footer-container top-container">
+      <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79a4-2bcc79a3" class="footer-col _1 top-col"><a href="/whats-on" aria-current="page" class="link-foo-big w--current">What’s on?</a>
+        <div class="foo-menu-div"><a href="#" class="link-foo-small">Schedule</a><a href="#" class="link-foo-small">Priority booking</a></div>
+        <div class="foo-marger"></div><a href="/festival" class="link-foo-whats_last w-inline-block"><img src="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%2039.jpg" loading="lazy" srcset="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%252039-p-500.jpeg 500w, https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%2039.jpg 576w" sizes="100vw" alt="" class="img-cover foters" />
+          <div class="text-block">Autumn Festival ‘22</div>
+        </a><a href="#" class="link-foo-small no-mob">Rachmaninov Days at Bechstein Hall</a>
+      </div>
+      <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79ba-2bcc79a3" class="footer-col top-col center-col"><a href="/your-visit" class="link-foo-big">your visit</a>
+        <div class="foo-menu-div"><a href="/box-office" class="link-foo-small">Health and safety</a></div>
+        <div class="foo-marger"></div>
+        <div class="foo-menu-div"><a href="/box-office" class="link-foo-small">Ticketing Info</a><a href="#" class="link-foo-small">Getting here</a><a href="#" class="link-foo-small">Security &amp; Rules</a><a href="#" class="link-foo-small">Contact Us</a></div>
+        <div class="foo-marger"></div>
+        <div class="foo-menu-div"><a href="/about" class="link-foo-small">Around Bechstein Hall</a><a href="#" class="link-foo-small">Tours</a><a href="#" class="link-foo-small">Eat &amp; drink</a><a href="#" class="link-foo-small">Venue &amp; seating plan</a></div>
+        <div class="foo-marger"></div>
+        <div class="foo-menu-div"><a href="#" class="link-foo-small">Accesible facilities</a><a href="#" class="link-foo-small">Accesibility statement</a><a href="/contacts" class="link-foo-small">Contacts</a></div>
+        <div class="foo-bottom no-pc"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+      </div>
+      <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79dd-2bcc79a3" class="footer-col _3 top-col"><a href="/history" class="link-foo-big _2">History</a><a href="#" class="link-foo-big _2">friends</a><a href="/press-office" class="link-foo-big _2">Press</a><a href="#" class="link-foo-big _2">hire the hall</a></div>
+    </div>
+    <div class="footer-container bottom">
+      <div class="footer-col _1">
+        <div class="div-block-2">
+          <div class="p-20-27">(020) 1234 5678 support@bechsteinhall.com London, W1U 2RJ, 22–28 Wigmore St.</div>
+        </div>
+        <div class="foo-soc-line"><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a></div>
+      </div>
+      <div class="footer-col no-mob">
+        <div class="foo-bottom nz"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+      </div>
+      <div class="footer-col _3">
+        <div class="foo-bottomer">
+          <div>© 2022, Bechstein Hall</div><a href="#" class="funk-link">Website made by Func. ↗</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="white-z"></div>
+</footer>
---
+++ header.php
@@ -0,0 +1,62 @@
+<div data-animation="default" data-collapse="none" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
+  <header class="header">
+    <div class="left-header"><a href="/" class="brand w-nav-brand"><img src="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc6105552615ba_logo.svg" loading="lazy" alt="" class="logo" /></a><a href="#" class="header-video-link w-inline-block"><img src="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc61a8292615b9_header-video-link.png" loading="lazy" alt="" /></a></div>
+    <div class="right-header">
+      <nav role="navigation" class="nav-menu w-nav-menu"><a href="/whats-on" aria-current="page" class="navbar-links w-nav-link w--current">What’s on</a><a href="/your-visit" class="navbar-links w-nav-link">your Visit</a><a href="#" class="navbar-links no-mob w-nav-link">bechstein hall</a></nav>
+      <div class="w-nav-button">
+        <div class="w-icon-nav-menu"></div>
+      </div>
+      <div class="r-head-sec">
+        <div class="right-menuer"><a href="#" class="head-search-btn w-inline-block"><img src="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc61ba862615bb_search.svg" loading="lazy" alt="" class="ico-60" /></a><a bgline="1" href="#" class="header-book-head-btn w-inline-block">
+            <div>Book tickets</div>
+          </a></div>
+        <div class="logreg"><a href="#" class="reglog-a">Log In</a>
+          <div> / </div><a href="#" class="reglog-a">Register</a>
+        </div><a href="#" class="b-menu w-inline-block">
+          <div class="b-line"></div>
+          <div class="b-line"></div>
+          <div class="b-line"></div>
+        </a>
+      </div>
+    </div>
+  </header>
+  <div class="burger-menu">
+    <div class="foo-mom">
+      <div class="footer-container top-container">
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79a4-2bcc79a3" class="footer-col _1 top-col"><a href="/whats-on" aria-current="page" class="link-foo-big w--current">What’s on?</a>
+          <div class="foo-menu-div"><a href="#" class="link-foo-small">Schedule</a><a href="#" class="link-foo-small">Priority booking</a></div>
+          <div class="foo-marger"></div><a href="/festival" class="link-foo-whats_last w-inline-block"><img src="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%2039.jpg" loading="lazy" srcset="https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%252039-p-500.jpeg 500w, https://uploads-ssl.webflow.com/62bc3fe7d9cc6134bf261592/62bc3fe7d9cc612f532615c7_Rectangle%2039.jpg 576w" sizes="100vw" alt="" class="img-cover foters" />
+            <div class="text-block">Autumn Festival ‘22</div>
+          </a><a href="#" class="link-foo-small no-mob">Rachmaninov Days at Bechstein Hall</a>
+        </div>
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79ba-2bcc79a3" class="footer-col top-col center-col"><a href="/your-visit" class="link-foo-big">your visit</a>
+          <div class="foo-menu-div"><a href="/box-office" class="link-foo-small">Health and safety</a></div>
+          <div class="foo-marger"></div>
+          <div class="foo-menu-div"><a href="/box-office" class="link-foo-small">Ticketing Info</a><a href="#" class="link-foo-small">Getting here</a><a href="#" class="link-foo-small">Security &amp; Rules</a><a href="#" class="link-foo-small">Contact Us</a></div>
+          <div class="foo-marger"></div>
+          <div class="foo-menu-div"><a href="/about" class="link-foo-small">Around Bechstein Hall</a><a href="#" class="link-foo-small">Tours</a><a href="#" class="link-foo-small">Eat &amp; drink</a><a href="#" class="link-foo-small">Venue &amp; seating plan</a></div>
+          <div class="foo-marger"></div>
+          <div class="foo-menu-div"><a href="#" class="link-foo-small">Accesible facilities</a><a href="#" class="link-foo-small">Accesibility statement</a><a href="/contacts" class="link-foo-small">Contacts</a></div>
+          <div class="foo-bottom no-pc"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+        </div>
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79dd-2bcc79a3" class="footer-col _3 top-col"><a href="/history" class="link-foo-big _2">History</a><a href="#" class="link-foo-big _2">friends</a><a href="/press-office" class="link-foo-big _2">Press</a><a href="#" class="link-foo-big _2">hire the hall</a></div>
+      </div>
+      <div class="footer-container bottom">
+        <div class="footer-col _1">
+          <div class="div-block-2">
+            <div class="p-20-27">(020) 1234 5678 support@bechsteinhall.com London, W1U 2RJ, 22–28 Wigmore St.</div>
+          </div>
+          <div class="foo-soc-line"><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a></div>
+        </div>
+        <div class="footer-col no-mob">
+          <div class="foo-bottom nz"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+        </div>
+        <div class="footer-col _3">
+          <div class="foo-bottomer">
+            <div>© 2022, Bechstein Hall</div><a href="#" class="funk-link">Website made by Func. ↗</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
