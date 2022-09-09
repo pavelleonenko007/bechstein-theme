693da0016650bde151933b25d08a8083d896b832
paveldonehq <pavel@donehq.com>
2022-09-09 16:21:29 +0300

    add search result page

--- custom_functions.php
+++ custom_functions.php
@@ -17,6 +17,53 @@ function bech_add_scripts()
   ));
 }
 
+/* Register Post Types */
+
+add_action('init', 'bech_register_post_types');
+function bech_register_post_types()
+{
+  register_post_type('your-visit', [
+    'label'  => null,
+    'labels' => [
+      'name'               => 'Your visit', // основное название для типа записи
+      'singular_name'      => 'Your visit', // название для одной записи этого типа
+      'add_new'            => 'Add visit', // для добавления новой записи
+      'add_new_item'       => 'Add new visit', // заголовка у вновь создаваемой записи в админ-панели.
+      'edit_item'          => 'Edit visit', // для редактирования типа записи
+      'new_item'           => 'New visit', // текст новой записи
+      'view_item'          => 'Watch visit', // для просмотра записи этого типа.
+      'search_items'       => 'Search visits', // для поиска по этим типам записи
+      'not_found'          => 'Visits not found', // если в результате поиска ничего не было найдено
+      'not_found_in_trash' => 'No found in trash', // если не было найдено в корзине
+      'parent_item_colon'  => '', // для родителей (у древовидных типов)
+      'menu_name'          => 'Your visit', // название меню
+    ],
+    'description'         => '',
+    'public'              => true,
+    // 'publicly_queryable'  => null, // зависит от public
+    // 'exclude_from_search' => null, // зависит от public
+    // 'show_ui'             => null, // зависит от public
+    // 'show_in_nav_menus'   => null, // зависит от public
+    'show_in_menu'        => true, // показывать ли в меню адмнки
+    // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+    'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+    'rest_base'           => null, // $post_type. C WP 4.7
+    'menu_position'       => null,
+    'menu_icon'           => 'dashicons-search',
+    //'capability_type'   => 'post',
+    //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+    //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+    'hierarchical'        => false,
+    'supports'            => ['title', 'editor', 'thumbnail'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+    'taxonomies'          => [],
+    'has_archive'         => true,
+    'rewrite'             => true,
+    'query_var'           => true,
+  ]);
+}
+
+/* Register Post Types */
+
 /* Tix Utils Functions */
 
 function bech_format_date($str = '', $format = 'Y-m-d H:i:s')
--- header.php
+++ header.php
@@ -1,34 +1,54 @@
 <div data-animation="default" data-collapse="none" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
   <header class="header">
-    <div class="left-header"><a href="/" aria-current="page" class="brand w-nav-brand w--current"><img src="<?php echo get_template_directory_uri() ?>/images/624c599c0b1f3739c684a6af_logo.svg" loading="lazy" alt class="logo"></a><a href="#" class="header-video-link w-inline-block"><img src="<?php echo get_template_directory_uri() ?>/images/624c599c9aa7a1be3a3e7425_header-video-link.png" loading="lazy" alt></a></div>
+    <div class="left-header">
+      <a href="/" aria-current="page" class="brand w-nav-brand w--current">
+        <img src="<?php echo get_template_directory_uri() ?>/images/624c599c0b1f3739c684a6af_logo.svg" loading="lazy" alt class="logo"></a>
+      <a href="#" class="header-video-link w-inline-block">
+        <img src="<?php echo get_template_directory_uri() ?>/images/624c599c9aa7a1be3a3e7425_header-video-link.png" loading="lazy" alt>
+      </a>
+    </div>
     <div class="right-header">
-      <nav role="navigation" class="nav-menu w-nav-menu"><a href="/whats-on" class="navbar-links w-nav-link">What’s on</a><a href="/your-visit" class="navbar-links w-nav-link">your Visit</a><a href="/history-copy" class="navbar-links no-mob w-nav-link">bechstein hall</a></nav>
+      <nav role="navigation" class="nav-menu w-nav-menu">
+        <a href="/whats-on" class="navbar-links w-nav-link">What’s on</a>
+        <a href="/your-visit" class="navbar-links w-nav-link">your Visit</a>
+        <a href="/history-copy" class="navbar-links no-mob w-nav-link">bechstein hall</a>
+      </nav>
       <div class="w-nav-button">
         <div class="w-icon-nav-menu"></div>
       </div>
       <div class="r-head-sec">
-        <div class="right-menuer"><a href="#" class="head-search-btn w-inline-block">
-            <div class="ico-60 w-embed"><svg width="60" height="60" viewbox="0 0 60 60" fill="none">
+        <div class="right-menuer">
+          <a href="#" class="head-search-btn w-inline-block">
+            <div class="ico-60 w-embed">
+              <svg width="60" height="60" viewbox="0 0 60 60" fill="none">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M36 26.5C36 32.299 31.299 37 25.5 37C19.701 37 15 32.299 15 26.5C15 20.701 19.701 16 25.5 16C31.299 16 36 20.701 36 26.5ZM34.8423 34.8051C32.5527 37.3788 29.2157 39 25.5 39C18.5964 39 13 33.4036 13 26.5C13 19.5964 18.5964 14 25.5 14C32.4036 14 38 19.5964 38 26.5C38 29.2354 37.1214 31.7656 35.6309 33.8237C35.7171 33.8235 35.8041 33.8371 35.8886 33.8652L37.3116 34.3396C37.4589 34.3887 37.5927 34.4714 37.7025 34.5812L46.0208 42.8995C46.4113 43.29 46.4113 43.9232 46.0208 44.3137L45.3137 45.0208C44.9232 45.4114 44.29 45.4114 43.8995 45.0208L35.5811 36.7025C35.4714 36.5927 35.3887 36.4589 35.3396 36.3116L34.8652 34.8886C34.856 34.861 34.8484 34.8331 34.8423 34.8051Z" fill="#030E14"></path>
-              </svg></div>
-          </a><a bgline="1" href="#" class="header-book-head-btn w-inline-block">
+              </svg>
+            </div>
+          </a>
+          <a bgline="1" href="#" class="header-book-head-btn w-inline-block">
             <div>Book tickets</div>
-          </a></div>
-        <div class="logreg"><a href="#" class="reglog-a">Log In</a>
-          <div> / </div><a href="#" class="reglog-a">Register</a>
-        </div><a href="#" class="b-menu w-inline-block">
+          </a>
+        </div>
+        <div class="logreg">
+          <a href="#" class="reglog-a">Log In</a>
+          <div> / </div>
+          <a href="#" class="reglog-a">Register</a>
+        </div>
+        <a href="#" class="b-menu w-inline-block">
           <div class="b-line"></div>
           <div class="b-line"></div>
           <div class="b-line"></div>
         </a>
       </div>
-      <form action="/search" class="searchhead w-form">
-        <input type="search" class="search-input w-input" autofocus="true" maxlength="256" name="query" placeholder=" search by keyword…" id="search-2" required>
-        <input type="submit" value="Search" class="search-button-2 w-button"><a href="#" class="head-search-btn-closen w-inline-block">
-          <div class="ico-60 w-embed"><svg width="32" height="31" viewbox="0 0 32 31" fill="none">
+      <form role="search" method="get" id="searchform" action="<?php echo home_url('/') ?>" class="searchhead">
+        <input type="search" class="search-input w-input" autofocus="true" maxlength="256" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder=" search by keyword…" required>
+        <input type="submit" id="searchsubmit" value="Search" class="search-button-2 w-button"><a href="#" class="head-search-btn-closen w-inline-block">
+          <div class="ico-60 w-embed">
+            <svg width="32" height="31" viewbox="0 0 32 31" fill="none">
               <line y1="-1" x2="40" y2="-1" transform="matrix(0.707107 0.707106 -0.707107 0.707106 2.00488 2.14209)" stroke="#030E14" stroke-width="2"></line>
               <line y1="-1" x2="40" y2="-1" transform="matrix(0.707107 -0.707106 0.707107 0.707106 2 30.2842)" stroke="#030E14" stroke-width="2"></line>
-            </svg></div>
+            </svg>
+          </div>
         </a>
       </form>
     </div>
@@ -36,28 +56,63 @@
   <div class="burger-menu">
     <div class="foo-mom">
       <div class="footer-container top-container">
-        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79a4-2bcc79a3" class="footer-col _1 top-col"><a href="/whats-on" class="link-foo-big">What’s on?</a><a href="#" class="link-foo-small">Schedule</a><a href="#" class="link-foo-small">Priority booking</a>
-          <div class="foo-marger"></div><a href="/festival" class="link-foo-whats_last w-inline-block"><img src="<?php echo get_template_directory_uri() ?>/images/625032fe74f84b30f4448559_Rectangle2039.jpg" loading="lazy" alt class="img-cover foters">
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79a4-2bcc79a3" class="footer-col _1 top-col">
+          <a href="/whats-on" class="link-foo-big">What’s on?</a>
+          <a href="#" class="link-foo-small">Schedule</a>
+          <a href="#" class="link-foo-small">Priority booking</a>
+          <div class="foo-marger"></div>
+          <a href="/festival" class="link-foo-whats_last w-inline-block">
+            <img src="<?php echo get_template_directory_uri() ?>/images/625032fe74f84b30f4448559_Rectangle2039.jpg" loading="lazy" alt class="img-cover foters">
             <div class="text-block">Autumn Festival ‘22</div>
-          </a><a href="#" class="link-foo-small no-mob">Rachmaninov Days at Bechstein Hall</a>
+          </a>
+          <a href="#" class="link-foo-small no-mob">Rachmaninov Days at Bechstein Hall</a>
         </div>
-        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79ba-2bcc79a3" class="footer-col top-col center-col"><a href="/your-visit" class="link-foo-big">your visit</a><a href="/box-office" class="link-foo-small">Health and safety</a>
-          <div class="foo-marger"></div><a href="/box-office" class="link-foo-small">Ticketing Info</a><a href="#" class="link-foo-small">Getting here</a><a href="#" class="link-foo-small">Security & Rules</a><a href="#" class="link-foo-small">Contact Us</a>
-          <div class="foo-marger"></div><a href="/about" class="link-foo-small">Around Bechstein Hall</a><a href="#" class="link-foo-small">Tours</a><a href="#" class="link-foo-small">Eat & drink</a><a href="#" class="link-foo-small">Venue & seating plan</a>
-          <div class="foo-marger"></div><a href="#" class="link-foo-small">Accesible facilities</a><a href="#" class="link-foo-small">Accesibility statement</a><a href="/contacts" class="link-foo-small">Contacts</a>
-          <div class="foo-bottom no-pc"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79ba-2bcc79a3" class="footer-col top-col center-col">
+          <a href="/your-visit" class="link-foo-big">your visit</a>
+          <a href="/box-office" class="link-foo-small">Health and safety</a>
+          <div class="foo-marger"></div>
+          <a href="/box-office" class="link-foo-small">Ticketing Info</a>
+          <a href="#" class="link-foo-small">Getting here</a>
+          <a href="#" class="link-foo-small">Security & Rules</a>
+          <a href="#" class="link-foo-small">Contact Us</a>
+          <div class="foo-marger"></div>
+          <a href="/about" class="link-foo-small">Around Bechstein Hall</a>
+          <a href="#" class="link-foo-small">Tours</a>
+          <a href="#" class="link-foo-small">Eat & drink</a>
+          <a href="#" class="link-foo-small">Venue & seating plan</a>
+          <div class="foo-marger"></div>
+          <a href="#" class="link-foo-small">Accesible facilities</a>
+          <a href="#" class="link-foo-small">Accesibility statement</a>
+          <a href="/contacts" class="link-foo-small">Contacts</a>
+          <div class="foo-bottom no-pc">
+            <a href="#" class="link-foo-small _2">Terms and Conditions</a>
+            <a href="#" class="link-foo-small last">Privacy policy</a>
+          </div>
         </div>
-        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79dd-2bcc79a3" class="footer-col _3 top-col"><a href="/history" class="link-foo-big _2">History</a><a href="#" class="link-foo-big _2">friends</a><a href="/press-office" class="link-foo-big _2">Press</a><a href="#" class="link-foo-big _2">hire the hall</a></div>
+        <div id="w-node-e1be876e-05a4-9245-628d-78602bcc79dd-2bcc79a3" class="footer-col _3 top-col">
+          <a href="/history" class="link-foo-big _2">History</a>
+          <a href="#" class="link-foo-big _2">friends</a>
+          <a href="/press-office" class="link-foo-big _2">Press</a>
+          <a href="#" class="link-foo-big _2">hire the hall</a>
+        </div>
       </div>
       <div class="footer-container bottom">
         <div class="footer-col _1">
           <div class="div-block-2">
             <div class="p-20-27">(020) 1234 5678 support@bechsteinhall.com London, W1U 2RJ, 22–28 Wigmore St.</div>
           </div>
-          <div class="foo-soc-line"><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a><a href="#" class="link-soc w-inline-block"></a></div>
+          <div class="foo-soc-line">
+            <a href="#" class="link-soc w-inline-block"></a>
+            <a href="#" class="link-soc w-inline-block"></a>
+            <a href="#" class="link-soc w-inline-block"></a>
+            <a href="#" class="link-soc w-inline-block"></a>
+          </div>
         </div>
         <div class="footer-col no-mob">
-          <div class="foo-bottom nz"><a href="#" class="link-foo-small _2">Terms and Conditions</a><a href="#" class="link-foo-small last">Privacy policy</a></div>
+          <div class="foo-bottom nz">
+            <a href="#" class="link-foo-small _2">Terms and Conditions</a>
+            <a href="#" class="link-foo-small last">Privacy policy</a>
+          </div>
         </div>
         <div class="footer-col _3">
           <div class="foo-bottomer">
